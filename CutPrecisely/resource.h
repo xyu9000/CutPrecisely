886a08e66122ede26409358fbadf715ccbb4a913
x9009 <92776817+x90009@users.noreply.github.com>
2022-04-14 20:55:27 -0700

    Add files via upload

---
+++ CutPrecisely/resource.h
@@ -0,0 +1,66 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 WindowsProject2.rc 使用
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDI_ICON1                       103
+#define IDD_DIALOG1                     105
+#define IDB_checked                     107
+#define IDB_indeterminate               108
+#define IDB_unchecked                   109
+#define IDC_EDIT_shuru                  1001
+#define IDC_EDIT_linshi                 1002
+#define IDC_EDIT_qishitime              1003
+#define IDC_EDIT_jieshutime             1004
+#define IDC_BUTTON_shuru                1005
+#define IDC_BUTTON_linshi               1006
+#define IDC_HWACCEL                     1007
+#define IDC_HWACCEL2                    1007
+#define IDC_BUTTON_step1                1008
+#define IDC_EDIT_dierbushuru            1009
+#define IDC_BUTTON_dierbushuru          1010
+#define IDC_EDIT_qishitime2             1011
+#define IDC_EDIT_shuchu                 1012
+#define IDC_BUTTON_shuchu               1013
+#define IDC_CHECK2                      1014
+#define IDC_HWACCEL1                    1014
+#define IDC_BUTTON_step2                1015
+#define IDC_EDIT_jieshutime2            1016
+#define IDC_BUTTON_gaiming              1017
+#define IDC_zhenlv                      1018
+#define IDC_qishizhen                   1019
+#define IDC_jieshuzhen                  1020
+#define IDC_ICQ                         1021
+#define IDC_ICQ2                        1021
+#define IDC_RW                          1022
+#define IDC_RH                          1023
+#define IDC_ICQ1                        1024
+#define IDC_BUTTON_shanchulinshi2       1025
+#define IDC_BUTTON_shanchulinshi        1025
+#define IDC_CMD                         1030
+#define IDC_EDIT1                       1031
+#define IDC_EDIT2                       1032
+#define IDC_BUTTON1                     1034
+#define IDC_CHECK1                      1035
+#define IDC_RADIO1                      1036
+#define IDC_RADIO2                      1037
+#define IDC_STATIC1                     1038
+#define IDC_SPIN1                       1039
+#define IDC_SPIN2                       1040
+#define IDC_biansu                      1041
+#define IDC_check                       1042
+#define IDC_CHECK4                      1045
+#define IDC_hw2                         1045
+#define IDC_FRAMES                      1046
+#define IDC_STATIC_dierbu               1047
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_CONTROL_VALUE         1050
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
